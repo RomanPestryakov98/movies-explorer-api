fc0ca16fdda7369330166e18809bc71655278b67
RomanPestryakov98 <95371899+RomanPestryakov98@users.noreply.github.com>
2022-09-15 12:23:24 +0300

    update cors

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
   const { origin } = req.headers;
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
   }
 
   next();
