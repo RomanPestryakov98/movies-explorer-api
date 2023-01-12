8a3de5a3f8610da1048ff5bc9b1c7a811eadaa4e
RomanPestryakov98 <95371899+RomanPestryakov98@users.noreply.github.com>
2023-01-12 13:44:18 +0300

    add cors

--- cors/allowedCors.js
+++ cors/allowedCors.js
@@ -1,3 +1,3 @@
-const allowedCors = ['https://moviesexplorer.nomoredomains.sb.nomoredomains.sbs', 'http://localhost:3000', 'http://moviesexplorer.nomoredomains.sb.nomoredomains.sbs', 'https://portfolio-roman.ru', 'http://portfolio-roman.ru'];
+const allowedCors = ['https://moviesexplorer.nomoredomains.sb.nomoredomains.sbs', 'http://localhost:3000', 'http://moviesexplorer.nomoredomains.sb.nomoredomains.sbs', 'https://movies-test.ru/', 'http://movies-test.ru/'];
 
 module.exports = allowedCors;
